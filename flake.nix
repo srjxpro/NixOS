a58e6a24caf56b67d62cee713dc87018a141002c
Suraj Sharma <229747370+srjxpro@users.noreply.github.com>
2025-09-21 00:31:31 +0530

    Flake Module

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        config.allowBroken = true;
+      };
+     # lib = home-manager.lib;
+    in {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.suraj = ./home.nix;
+
+              home-manager.extraSpecialArgs = {
+                inherit pkgs;
+              };
+            }
+          ];
+        };
+      };
+    };
+}
